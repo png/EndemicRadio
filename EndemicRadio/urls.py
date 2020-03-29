579c6f92c2cb1b1be7e9f2cef459d6e670f74047
png <jasona99@outlook.com>
2020-03-29 13:11:01 -0400

    Various more merge fixes. Darn you, git!

--- EndemicRadio/urls.py
+++ EndemicRadio/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include,path
 from django.conf import settings
 from django.conf.urls.static import static
-from stream.views import wikipediaUpdate, getPlaylistByRegion
 
 
 
@@ -25,6 +24,4 @@ from stream.views import wikipediaUpdate, getPlaylistByRegion
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('stream.urls')),
-    path('wikipediaUpdate/', wikipediaUpdate),
-    path('playlist/<slug:regionName>', getPlaylistByRegion)
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- stream/urls.py
+++ stream/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('musicpage/', views.musicpage, name='musicpage'),
     path('player/', views.player, name='player'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.logout, name='logout'),
+    path('wikipediaUpdate/', views.wikipediaUpdate),
+    path('playlist/<slug:regionName>', views.getPlaylistByRegion)
 ]
